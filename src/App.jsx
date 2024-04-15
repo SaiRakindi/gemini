02a67c343f8abfa4ee8108addf5024dc18cfc484
SaiRakindi <sai.rakindi3@gmail.com>
2024-04-15 16:22:58 +0530

    feat: main component added and styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Main from "./components/Main/Main";
 import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
-  return <Sidebar />;
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
 };
 
 export default App;
