c6eff7ac5f0626eb8f588218f58866305b27a03e
SaiRakindi <sai.rakindi3@gmail.com>
2024-04-18 22:58:02 +0530

    feat: markdown response is handled and shifted to new line and strong words

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -11,12 +11,27 @@ const ContextProvider = (props) => {
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
+  const delayPara = (index, nextWord) => {};
+
   const onSent = async (prompt) => {
     setResultData("");
     setLoading(true);
     setShowResult(true);
+    setRecentPrompt(input);
+    setInput("");
     const response = await runChat(input);
-    setResultData(response);
+    let responseArray = response.split("**");
+    let newResponse;
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+
+    let newResponse2 = newResponse.split("*").join("</br>");
+    setResultData(newResponse2);
     setLoading(false);
     setInput("");
   };
